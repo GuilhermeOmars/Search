b5ccfdb4b94c8789b7e18fae8da7654417e355e4
GuilhermeOmars <guilhermeomar.silva@gmail.com>
2024-08-25 00:01:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const search = () => {
-    const string = document.getElementById('Search').value.toLowerCase();
-    const titulos = document.querySelectorAll('#Titulo');
+    const string = document.getElementById('Search').value.toLowerCase()
+    const titulos = document.querySelectorAll('#Titulo')
     for (let i = 0; i < titulos.length; i++) {
-        const titulo = titulos[i].textContent.toLowerCase();
+        const titulo = titulos[i].textContent.toLowerCase()
         if (titulo.includes(string)) {
-            console.log(`O título "${titulo}" contém a string "${string}"`);
+            console.log(`O título "${titulo}" contém a string "${string}"`)
         }
     }
 };
